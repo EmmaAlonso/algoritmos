627c332caaa66f9df5c7f61b7577226275d3dcc7
EmmaAlonso <159556204+EmmaAlonso@users.noreply.github.com>
2024-02-26 23:19:32 -0600

    Add files via upload
    funciones HTML

---
+++ expresiones.js
@@ -0,0 +1,121 @@
+function saludar () {
+    const nombre = "Alonso";
+    console.log("Hola", nombre);
+}
+
+function saludardos () {
+const nombreDos = prompt("ingresa nombre")
+console.log("Hola", nombreDos);
+}
+
+function suma () {
+const suma = "resultado"
+console.log("resultado", 5 + 5)
+}
+
+function division (){
+console.log ("resultado", 10/5)
+}
+
+function multiplicacion () {
+console.log ("resultado", 9*9)
+}
+
+function resta () {
+console.log("resultado", 50-20)
+}
+
+function despedir () {
+const nombre = "Emmanuel";
+console.log("Adios", nombre)
+}
+
+function preguntar () {
+const nombre = "Emmanuel"
+console.log("¿Como estas?", nombre)
+}
+
+function areacuadrado () {
+const formula = "L*L"
+console.log("10*10", 10*10)
+}
+
+function areatriangulo () {
+    const formula = "b*h /2"
+    console.log("10*5 /2", 10*5 /2)
+}
+
+function preguntados () {
+    const nombredos = prompt ("ingresa nombre")
+    console.log("¿Como estas?", nombredos)
+}
+
+function arearectangulo () {
+    const formula = "b*a"
+console.log(formula ,"8*5", 8*5)
+}
+
+function despedirdos () {
+const nombredos = prompt ("ingresa nombre")
+console.log("Adios",nombredos)
+}
+
+function sumados () {
+const suma = ("1+4+5+7")
+console.log("resultado",1+4+5+7)
+}
+
+function multiplicaciondivision () {
+    const resultado = "25*5/2"
+    console.log(resultado, 25*5/2)
+}
+
+function restadivision () {
+    const resultado = "100-20 / 2"
+    console.log(resultado, 100-20/ 2);
+}
+
+function divisionsuma () {
+    const resultado = "50/2 + 10"
+    console.log(resultado, 50/2 + 10)
+}
+
+function restasuma () {
+    const resultado = "70-20 + 15"
+    console.log(resultado, 70-20 + 15)
+}
+
+function multiplicacionsuma () {
+    const resultado = "10*10 +30"
+    console.log(resultado, 10*10 + 30)
+}
+
+function restamultiplicacion () {
+    const resultado = "200-30 * 2"
+    console.log(resultado, 200-30 * 2)
+}
+
+function divisionresta () {
+    const resultado = "20/2 - 3"
+    console.log(resultado, 20/2 - 3)
+}
+
+function sumaresta () {
+    const resultado = "100+100 - 50"
+    console.log(resultado, 100+100 - 50)
+}
+
+function sumamultiplicacion () {
+    const resultado = "20+30 * 100"
+    console.log(resultado, 20+30 * 100)
+}
+
+function divisionmultiplicacion () {
+    const resultado = "60/3 * 5"
+    console.log(resultado, 60/3 * 5)
+}
+
+function sumadivision () {
+    const resultado = "70+70 / 5"
+    console.log(resultado, 70+70 / 5)
+}
